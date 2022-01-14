39be4668e50a056501f7c19ac8d516e2f70750e2
Maxim Chernyshov <dsgnrvd@gmail.com>
2022-01-15 00:40:28 +0300

    improve test code, add properly slots test

--- src/components/LayoutTemplate.spec.ts
+++ src/components/LayoutTemplate.spec.ts
@@ -1,6 +1,6 @@
-import LayoutTemplate from './LayoutTemplate.ce.vue'
+import LayoutTemplate from './LayoutTemplate.ce.vue';
 import { mount } from '@vue/test-utils';
-import { h, RendererElement, RendererNode, VNode } from 'vue'
+import { h, RendererElement, RendererNode, VNode } from 'vue';
 
 type Slots = {
     slots: {
@@ -15,27 +15,44 @@ const provideMountParams = (slots: Slots) => {
 }
 
 describe('Layout', () => {
+    const hHeader = h('div', 'header slot');
+    const hContent = h('div', 'content slot');
+    const hFooter = h('div', 'footer slot');
+    const headerNode = '<div>header slot</div>';
+    const contentNode = '<div>content slot</div>';
+    const footerNode = '<div>footer slot</div>';
+    const nextLineNode = '\n';
+    const mergedSlotNodes = headerNode + nextLineNode + contentNode + nextLineNode + footerNode;
+
     test('Header slot exists', () => {
-        const slots = { slots: { header: () => h('div', 'header slot')} };
+        const slots = { slots: { header: () => hHeader } };
         
         const wrapper = provideMountParams(slots);
   
-        expect(wrapper.html()).toBe('<div>header slot</div>');
+        expect(wrapper.html()).toBe(headerNode);
     })
 
     test('Content slot exists', () => {
-      const slots = { slots: { content: () => h('div', 'content slot')} };
+      const slots = { slots: { content: () => hContent } };
       
       const wrapper = provideMountParams(slots);
 
-      expect(wrapper.html()).toBe('<div>content slot</div>');
+      expect(wrapper.html()).toBe(contentNode);
     })
 
     test('Footer slot exists', () => {
-        const slots = { slots: { content: () => h('div', 'footer slot')} };
+        const slots = { slots: { footer: () => hFooter } };
         
         const wrapper = provideMountParams(slots);
   
-        expect(wrapper.html()).toBe('<div>footer slot</div>');
+        expect(wrapper.html()).toBe(footerNode);
+    })
+
+    test('Prev slots works together properly', () => {
+        const slots = { slots: { header: () => hHeader, content: () => hContent, footer: () => hFooter } };
+        
+        const wrapper = provideMountParams(slots);
+        
+        expect(wrapper.html()).toBe(mergedSlotNodes);
     })
 })
