879f394d47eda63b9eba9b0f20c1529ce3e35101
Maxim Chernyshov <dsgnrvd@gmail.com>
2022-01-15 00:30:14 +0300

    add tests of other slots of LayoutElement

--- src/components/LayoutTemplate.spec.ts
+++ src/components/LayoutTemplate.spec.ts
@@ -1,11 +1,41 @@
 import LayoutTemplate from './LayoutTemplate.ce.vue'
 import { mount } from '@vue/test-utils';
-import { h } from 'vue'
+import { h, RendererElement, RendererNode, VNode } from 'vue'
+
+type Slots = {
+    slots: {
+        [key: string]: () => VNode<RendererNode, RendererElement, {
+            [key: string]: typeof h;
+        }>;
+    };
+};
+
+const provideMountParams = (slots: Slots) => {
+    return mount(LayoutTemplate, slots);
+}
 
 describe('Layout', () => {
+    test('Header slot exists', () => {
+        const slots = { slots: { header: () => h('div', 'header slot')} };
+        
+        const wrapper = provideMountParams(slots);
+  
+        expect(wrapper.html()).toBe('<div>header slot</div>');
+    })
+
     test('Content slot exists', () => {
-      const wrapper = mount(LayoutTemplate, { slots: { content: () => h('div', 'content slot')} });
+      const slots = { slots: { content: () => h('div', 'content slot')} };
+      
+      const wrapper = provideMountParams(slots);
 
       expect(wrapper.html()).toBe('<div>content slot</div>');
+    })
+
+    test('Footer slot exists', () => {
+        const slots = { slots: { content: () => h('div', 'footer slot')} };
+        
+        const wrapper = provideMountParams(slots);
+  
+        expect(wrapper.html()).toBe('<div>footer slot</div>');
     })
 })
