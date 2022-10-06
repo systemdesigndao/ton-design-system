86b29a2ef94826018fe40ee9db7a576241fad595
Maxim Chernyshov <dsgnrvd@gmail.com>
2022-10-07 02:47:12 +0400

    remove unused preview, batch css in new version

--- vite.config.ts
+++ vite.config.ts
@@ -2,30 +2,10 @@ import { defineConfig, UserConfigExport } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path';
 
-const htmlPlugin = () => {
-  return {
-    name: 'html-transform',
-    transformIndexHtml(html) {
-      // postcss inject in dev and prod, without that vite preview doesnt working ;(
-      const pathToStyle = process.env.NODE_ENV === 'development' ? 'src/index.css' : '/style.css';
-      return html.replace(
-        /<title>(.*?)<\/title>/,
-        `<link rel="stylesheet" href="${pathToStyle}">
-        <link rel="preload" as="font" type="font/ttf" href="/fonts/Mulish-Regular.ttf" crossorigin="">
-        <link rel="preload" as="font" type="font/ttf" href="/fonts/Mulish-Black.ttf" crossorigin="">
-        <link rel="preload" as="font" type="font/ttf" href="/fonts/Mulish-ExtraBold.ttf" crossorigin="">
-        <link rel="preload" as="font" type="font/ttf" href="/fonts/Mulish-Bold.ttf" crossorigin="">
-        <title>Vite App</title>`
-      )
-    }
-  }
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    htmlPlugin(),
   ],
   build: {
     lib: {
