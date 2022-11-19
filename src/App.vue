43963461169a3533b0f227bd3922dc6aea03780a
Maxim Chernyshov <dsgnrvd@gmail.com>
2022-11-19 15:09:36 +0400

    removed variables from ton.org because of unused

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,5 @@
 <script setup lang="ts">
 /*
-    // take from ton.org
-    --background_light_icon: rgba(0,136,204,0.06);
-    --background_light_blue: rgba(0,136,204,0.1);
-    --background_dark_main: #232328;
-    --background_dark_gradient: linear-gradient(0deg,#232328,#343437 101.47%);
-    --background_dark_secondary: hsla(0,0%,100%,0.03);
-    --background_gradient_light: linear-gradient(180deg,#fff,#f7f9fb 134.8%);
-    --background_green_light: #829a94;
-    --background_black_mini_opacity: rgb(0,0,0,0.24);
-    --background_loading_gradient_light: linear-gradient(90deg,#f7f9fb -9.69%,#f2f5f8 -9.68%,#fff 52.19%,#f2f5f8 106.56%);
-    --button_light_primary: #08c;
-    --button_light_primary_hover: #0197e2;
-    --button_light_primary_pressed: #0082c2;
-    --button_light_secondary: rgba(246,248,251,0.8);
-    --button_light_secondary_hover: #f4f7fa;
-    --button_light_secondary_pressed: #f4f7fa;
-    --button_dark_primary: #08c;
-    --button_dark_primary_hover: #00a1f1;
-    --button_dark_primary_pressed: #076c9f;
-    --button_dark_secondary: hsla(0,0%,100%,0.06);
-    --button_dark_secondary_hover: hsla(0,0%,100%,0.12);
-    --button_dark_secondary_pressed: hsla(0,0%,100%,0.03);
-    --button_dark_text: #07a0ec;
-    --button_dark_secondary_text_hover: #5bc8ff;
-    --button_dark_secondary_text_pressed: #0186c9;
-    --button_light_primary_small: rgba(0,136,204,0.06);
-    --button_light_primary_small_hover: rgba(0,136,204,0.3);
-    --button_light_primary_small_pressed: rgba(0,136,204,0.03);
-    --text_light_primary: #161c28;
-    --text_light_secondary: #728a96;
-    --text_dark_primary: #fff;
-    --text_dark_secondary: hsla(0,0%,100%,0.7);
-    --text_dark_gradient: linear-gradient(89.92deg,#06a1ef 28.51%,#69cdff 85.79%);
-    --icon_light_primary: #08c;
-    --icon_light_secondary: rgba(236,240,243,0.8);
-    --icon_light_thirdly: #98b2bf;
-    --icon_dark_primary: #02a8fb;
-    --separator_light: rgba(123,148,160,0.2);
-    --separator_dark: hsla(0,0%,100%,0.06);
-    --black: #000;
-    --covers_light_green: #caefd9;
-    --hovered_link: #08c;
-    --light_border: rgba(114,138,150,0.24);
-
-    --ton_blue: #08c;
-    --ton_dark_blue: #019be9;
-    --default_white: #fff;
-    --default_black: #161c28;
-    --toncoin_header: #353538;
-    --toncoin_gradient: linear-gradient(297.97deg,#232328 9.93%,#343437 76.88%);
-*/
-
-/*
 Example of preferred color scheme supporting feature
 className="bg-grayLight dark:bg-grayDark"
 */
