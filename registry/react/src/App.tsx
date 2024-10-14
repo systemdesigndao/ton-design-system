994591d5cf7673c1ecac725854cd444f541788b5
Maxim <dsgnrvd@gmail.com>
2024-10-14 22:42:54 +0300

    react 1e4 items

--- registry/react/src/App.tsx
+++ registry/react/src/App.tsx
@@ -1,9 +1,19 @@
-import { Button } from "./components/Button"
+import { useState } from "react";
 
+const Like = () => {
+  const [count, setCount] = useState(0);
+  return <div>
+    <span>❤️</span>
+    {count}
+    <button onClick={() => {
+      setCount(prev => ++prev);
+    }}>+</button>
+  </div>
+}
 const App = () => {
   return (
-    <div className='px-2 py-2 h-full flex'>
-      <Button className="custom-classname">Cross-platform button (default)</Button>
+    <div className='px-2 py-2 h-full flex flex-col'>
+      {Array.from({ length: 1e4 }).map((_, index) => <Like key={`like-component-${index}`} />)}
     </div>
   );
 };
